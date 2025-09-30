861f8808be8526c2a20ebb5de9eeb226f25d93c7
Mike D'Arcy <miked@allenai.org>
2025-09-30 13:45:26 -0700

    Add oneshot solver

--- agent_baselines/solvers/llm.py
+++ agent_baselines/solvers/llm.py
@@ -1,4 +1,20 @@
-from inspect_ai.solver import Solver, chain, generate, solver, system_message
+import logging
+
+from inspect_ai.model import (
+    ChatMessageSystem,
+    get_model,
+)
+from inspect_ai.solver import (
+    Generate,
+    Solver,
+    TaskState,
+    chain,
+    generate,
+    solver,
+    system_message,
+)
+
+logger = logging.getLogger(__name__)
 
 
 @solver
@@ -15,3 +31,25 @@ def llm_with_prompt(system_prompt: str | None = None) -> Solver:
         chainlist.insert(0, system_message(system_prompt))
 
     return chain(chainlist)
+
+
+@solver
+def onestep_llm_solver() -> Solver:
+    async def solve(state: TaskState, generate: Generate) -> TaskState:
+        # state.messages is seeded with the task input as a ChatMessageUser;
+        # `ChatMessageUser(content=state.input_text)` would achieve basically
+        # the same thing
+        state.messages.insert(
+            0, ChatMessageSystem(content="You are a helpful assistant")
+        )
+
+        # Could use `generate` param but it's less flexible; that takes
+        # a `TaskState`, while `get_model().generate` takes a message list
+        m = get_model()
+        response = await m.generate(state.messages, tools=state.tools)
+        state.output.completion = response.choices[0].message.content
+
+        logger.info(f"Input: {state.input_text}\nOutput: {state.output.completion}")
+        return state
+
+    return solve
